d4b63ad15208718f7b52365c813bf081be85eb86
rbi13 <ronbond13@gmail.com>
2015-07-25 23:07:58 -0400

    python 3 compatibility

--- dynamic.py
+++ dynamic.py
@@ -2,14 +2,17 @@
 ## 
 ## dynamic.py
 
-def loadClass(cl):
+def load_class(cl):
     d = cl.rfind(".")
-    classname = cl[d+1:len(cl)]
+    classname = cl[d + 1:len(cl)]
+    classname = classname.encode('ascii', 'ignore')
     m = __import__(cl[0:d], globals(), locals(), [classname])
     return getattr(m, classname)
 
-def callMethod(obj,method,args):
-	return getattr(obj, method)(*args)
 
-def testImport():
-	print "Dynamic Imported"
+def call_method(obj, method, args):
+    return getattr(obj, method)(*args)
+
+
+def test_import():
+    print("Dynamic Imported")
